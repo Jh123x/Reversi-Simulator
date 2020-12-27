02fa3ac0f3700f3a775cfa20fc0e2a33527345c1
Junhua <wenjh1998@gmail.com>
2020-12-28 00:09:18 +0800

    Add Exception Classes

---
+++ Core/Exceptions/GeneralException.py
@@ -0,0 +1,4 @@
+class GeneralException(Exception):
+    def __init__(self, message:str):
+        """The general exception for the game"""
+        super().__init__(message)
---
+++ Core/Exceptions/InvalidPositionException.py
@@ -0,0 +1,5 @@
+from .GeneralException import GeneralException
+
+class InvalidPositionException(GeneralException):
+    def __init__(self, message:str):
+        super().__init__(f"Invalid position {message}")
