0d87d34c1eb00e1436ba08a13228f2d3f2673a89
Wen Junhua <wenjh1998@gmail.com>
2020-12-28 06:14:31 +0000

    Update skeleton of GUI

---
+++ GUI/UI.py
@@ -0,0 +1,10 @@
+
+class GUI(object):
+    def __init__(self):
+        """GUI window to run the game"""
+        super().__init__()
+    
+    def mainloop(self):
+        """Main loop to run the GUI"""
+        raise NotImplementedError("Currently not implemented yet")
+    
--- __main__.py
+++ __main__.py
@@ -1,11 +1,17 @@
 import tkinter as tk
 from CLI.UI import CLI
+from GUI.UI import GUI
 from Core.Index import Index
 
 def run_cli():
     """Run the command line version of the game"""
     cli_ui = CLI()
     cli_ui.mainloop()
+
+def run_gui():
+    """Run the game in GUI mode TODO"""
+    gui_ui = GUI()
+    gui_ui.mainloop()
 
 if __name__ == "__main__":
     run_cli()
