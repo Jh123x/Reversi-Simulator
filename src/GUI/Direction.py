9e7e987e8bc3a7d153c99dd2ddb5b9df284a964b
Wen Junhua <wenjh1998@gmail.com>
2021-01-02 00:36:58 +0800

    Bug Fix for directions

--- src/GUI/Direction.py
+++ src/GUI/Direction.py
@@ -1,4 +1,4 @@
-import enum
+from enum import Enum
 from pygame.rect import Rect
 
 
@@ -8,26 +8,26 @@ def center(rect: Rect, x: int, y: int) -> Rect:
 
 
 def left(rect: Rect, x: int, y: int) -> Rect:
-    rect.left = (x, y)
+    rect.center = (x + rect.width / 2, y)
     return rect
 
 
 def right(rect: Rect, x: int, y: int) -> Rect:
-    rect.right = (x, y)
+    rect.center = (x - rect.width / 2, y)
     return rect
 
 
 def up(rect: Rect, x: int, y: int) -> Rect:
-    rect.top = (x, y)
+    rect.center = (x, y - rect.height / 2)
     return rect
 
 
 def down(rect: Rect, x: int, y: int) -> Rect:
-    rect.bottom = (x, y)
+    rect.center = (x, y + rect.height / 2)
     return rect
 
 
-class DIRECTION(enum):
+class DIRECTION(Enum):
     CENTER = center
     LEFT = left
     RIGHT = right
