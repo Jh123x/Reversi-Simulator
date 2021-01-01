c2153249b8b78a5540caa9814e3b7eb1abc37843
Wen Junhua <wenjh1998@gmail.com>
2021-01-01 12:13:01 +0800

    Move files to src folder

--- src/__main__.py
+++ src/__main__.py
@@ -6,18 +6,18 @@ from argparse import ArgumentParser
 
 if __name__ == "__main__":
 
-    #Create parser
+    # Create parser
     parser = ArgumentParser()
     parser.add_argument('-gui', "--gui", action="store_true", help="Run the game in GUI mode")
 
-    #Parse arguments
+    # Parse arguments
     args = parser.parse_args()
 
-    #Create the board
+    # Create the board
     board = GameBoard()
 
     runner = CLI(board)
-    if(args.gui):
+    if args.gui:
         runner = GUI(board, 800, 600)
         
     runner.mainloop()
