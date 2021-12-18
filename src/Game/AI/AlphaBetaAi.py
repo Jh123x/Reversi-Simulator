16e81730d54088bbd0594cca2ece789a74ca4923
Wen Junhua <wenjh1998@gmail.com>
2021-12-18 20:13:58 +0800

    style: update linting

--- src/Game/AI/AlphaBetaAi.py
+++ src/Game/AI/AlphaBetaAi.py
@@ -1,14 +1,11 @@
-from numpy import ndarray
 from Core.Constants import AI_MOVE_KEY
 from Core.Index import Index
 from Game.AI.Ai import AI
 from Game.Board import GameBoard
 from Game.PlayerEnum import PlayerTurn
 
-class AlphaBetaAi(AI):
-    def __init__(self):
-        super().__init__()
 
+class AlphaBetaAi(AI):
     def get_next_turn(self, current_turn: PlayerTurn):
         PlayerTurn.BLACK if current_turn == PlayerTurn.WHITE else PlayerTurn.WHITE
 
@@ -24,15 +21,16 @@ class AlphaBetaAi(AI):
         if depth == 0 or len(valid_positions) == 0:
             black, white = board.get_score()
             return black - white
-        
+
         # Alpha for black
         if current_turn == PlayerTurn.BLACK:
             value = -float("inf")
             for position in valid_positions:
                 cpy_board = GameBoard(board)
-                x , y = self.to_index(*position)
+                x, y = self.to_index(*position)
                 cpy_board.place(x, y)
-                value = max(value, self.alphaBetaPrune(cpy_board, depth - 1, alpha, beta))
+                value = max(value, self.alphaBetaPrune(
+                    cpy_board, depth - 1, alpha, beta))
                 if value >= beta:
                     break
                 alpha = max(alpha, value)
@@ -42,9 +40,10 @@ class AlphaBetaAi(AI):
         value = float("inf")
         for position in valid_positions:
             cpy_board = GameBoard(board)
-            x , y = self.to_index(*position)
-            cpy_board.place(x , y)
-            value = max(value, self.alphaBetaPrune(cpy_board, depth - 1, alpha, beta))
+            x, y = self.to_index(*position)
+            cpy_board.place(x, y)
+            value = max(value, self.alphaBetaPrune(
+                cpy_board, depth - 1, alpha, beta))
             if value <= alpha:
                 break
             alpha = min(alpha, value)
@@ -56,7 +55,6 @@ class AlphaBetaAi(AI):
         """
         return Index.from_zero_based(x), Index.from_zero_based(y)
 
-    
     def get_move(self, board: GameBoard, result_dict: dict = None) -> tuple[int, int]:
         """
         Returns the position of the move that the AI will make.
@@ -66,13 +64,14 @@ class AlphaBetaAi(AI):
         assert current_turn == PlayerTurn.WHITE
         curr_contender = None
         curr_score = None
-        
+
         # Start the alpha beta search
         for position in board.get_valid_positions().keys():
             cpy = GameBoard(board)
-            x , y = self.to_index(*position)
+            x, y = self.to_index(*position)
             cpy.place(x, y)
-            score = self.alphaBetaPrune(cpy, depth=6, alpha=-float("inf"), beta=float("inf"))
+            score = self.alphaBetaPrune(
+                cpy, depth=6, alpha=-float("inf"), beta=float("inf"))
 
             if curr_contender is None or score < curr_score:
                 curr_contender = position
@@ -82,7 +81,3 @@ class AlphaBetaAi(AI):
             result_dict[AI_MOVE_KEY] = curr_contender
 
         return curr_contender
-            
-
-            
-        
